e6d805b1fa230133650f0c75e1580ef80289bb3a
Philip Woods <elzairthesorcerer@gmail.com>
2017-10-15 18:00:20 -0400

    Added test to library and added top-level error handling

--- src/bin/gltf2wg3d.rs
+++ src/bin/gltf2wg3d.rs
@@ -1,4 +1,23 @@
+extern crate gltf;
 extern crate wg3d;
 
+use std::fs::File;
+use std::io;
+use std::path::Path;
+use gltf::Gltf;
+
 fn main() {
+    let file = File::open("testmodels/gltf/model.gltf").ok().unwrap();
+    let gltf = Gltf::from_reader(io::BufReader::new(file)).ok().unwrap()
+        .validate_minimally().ok().unwrap();
+
+    for scene in gltf.scenes() {
+        for node in scene.nodes() {
+            println!(
+                "Node {} has {} children",
+                node.index(),
+                node.children().count(),
+            );
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,80 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-pub fn test() -> u64 {
-    5_u64
+use std::error;
+use std::fmt;
+use std::fs::File;
+use std::io;
+use std::result;
+
+use gltf::Gltf;
+
+pub fn test() -> Result<()> {
+    let file = File::open("testmodels/gltf2/Box/Box.gltf")?;
+    let gltf = Gltf::from_reader(io::BufReader::new(file))?
+        .validate_minimally()?;
+
+    println!("Got here!");
+
+    for scene in gltf.scenes() {
+        for node in scene.nodes() {
+            println!(
+                "Node {} has {} children",
+                node.index(),
+                node.children().count(),
+            );
+        }
+    }
+
+    Ok(())
 }
+
+/// This is the top level Error for this crate.
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    Gltf(gltf::Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Error::Io(ref err) => err.fmt(fmt),
+            &Error::Gltf(ref err) => err.fmt(fmt),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match self {
+            &Error::Io(ref err) => err.description(),
+            &Error::Gltf(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match self {
+            &Error::Io(ref err) => err.cause(),
+            &Error::Gltf(ref err) => err.cause(),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl From<gltf::Error> for Error {
+    fn from(err: gltf::Error) -> Error {
+        Error::Gltf(err)
+    }
+}
+
+/// This is the result type.
+pub type Result<T> = result::Result<T, Error>;
 
 #[cfg(test)]
 mod tests {
@@ -14,6 +85,12 @@ mod tests {
     
     #[test]
     fn it_works() {
-        assert_eq!(test(), 5_u64);
+        match test() {
+            Ok(_) => {},
+            Err(err) => {
+                println!("{}", err.to_string());
+                assert!(false);
+            }
+        }
     }
 }
